46e6f141c5bdbd5212cbb67df7c9e0904a9d53f3
BigBullAshuu <96278505+BigBullAshuu@users.noreply.github.com>
2021-12-17 10:24:02 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,8 +9,8 @@ const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
     <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <p>Welcome to Shiv's new business site.</p>
+    <p>Now I will build something great for my business.</p>
     <StaticImage
       src="../images/gatsby-astronaut.png"
       width={300}
@@ -20,10 +20,10 @@ const IndexPage = () => (
       style={{ marginBottom: `1.45rem` }}
     />
     <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link> <br />
-      <Link to="/using-ssr">Go to "Using SSR"</Link> <br />
-      <Link to="/using-dsg">Go to "Using DSG"</Link>
+      <Link to="/page-2/">Go to business 1</Link> <br />
+      <Link to="/using-typescript/">Go to business 2"Using TypeScript"</Link> <br />
+      <Link to="/using-ssr">Go to three "Using SSR"</Link> <br />
+      <Link to="/using-dsg">Go to four "Using DSG"</Link>
     </p>
   </Layout>
 )
